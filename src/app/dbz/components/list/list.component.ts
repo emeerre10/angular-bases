a01818032f9f73185bb7a5213b81a8564b6ee082
emeerre10 <mrmedina.is.m@gmail.com>
2023-11-13 20:18:51 +0100

    Add services

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Character } from '../../interfaces/character.interface';
 
 @Component({
@@ -10,4 +10,13 @@ export class ListComponent {
 
   @Input()
   public characterList: Character[] = [];
+
+  @Output()
+  public onDeleteCharacter: EventEmitter<string> = new EventEmitter();
+
+  emitId(id?: string): void {
+    if (!id) return;
+    this.onDeleteCharacter.emit(id);
+  }
+
 }
--- src/app/dbz/pages/main-pages.component.ts
+++ src/app/dbz/pages/main-pages.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { Character } from "../interfaces/character.interface";
+import { DbzService } from "../services/dbz.service";
 
 @Component({
     selector: 'app-dbz-main-page',
@@ -8,18 +9,17 @@ import { Character } from "../interfaces/character.interface";
 
 export class MainPagesComponent {
 
-    public characters: Character[] = [
-        {
-            name: 'Krillin',
-            power: 1000
-        },
-        {
-            name: 'Goku',
-            power: 9500
-        },
-        {
-            name: 'Vegeta',
-            power: 7500
-        }
-    ];
+    constructor(private dbzService: DbzService) {}
+
+    get characters(): Character[] {
+        return [...this.dbzService.characters];
+    }
+
+    deleteById(id: string): void {
+        this.dbzService.deleteById(id);
+    }
+
+    save(character: Character): void {
+        this.dbzService.save(character);
+    }
 }
